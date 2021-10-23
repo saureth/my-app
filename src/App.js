76f16d284a068903ae1880c2bfe6746551d2a276
Santiago Sanmartín <saureth001@gmail.com>
2021-10-23 18:58:15 -0500

    Clase 28

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import Button from "./button";
 
+const arr=[
+  'Chanchito feliz',
+  'Chanchito triste',
+  'Chanchito emocionado'
+];
 const App = ()=>{
-  const miVariable = true;
+  const miVariable = false;
   if(miVariable){
     return <p>Variable en {miVariable.toString()}</p>
   }
   return (
     <div>
       <h1 onClick={(e)=>console.log('click', e)}>Hola Mundo</h1>
+      {arr.map(el => <p key={el}>{el}</p>)}
       <Button onClick={()=>console.log('clickeado')}>
         Enviar
       </Button>
