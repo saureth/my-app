90f80320cf9b81ae65a548ea1f22cbcd7c4a6291
Santiago Sanmartín <saureth001@gmail.com>
2021-10-23 18:51:28 -0500

    Clase 26

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Button from "./button";
 const App = ()=>{
   return (
     <div>
-      <h1>Hola Mundo</h1>
+      <h1 onClick={(e)=>console.log('click', e)}>Hola Mundo</h1>
       <Button onClick={()=>console.log('clickeado')}>
         Enviar
       </Button>
