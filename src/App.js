59733e6f075ed02c819ad1f20ce7193e072e9e4a
Santiago Sanmartín <saureth001@gmail.com>
2021-10-23 19:28:04 -0500

    Clase 32

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import { Component } from 'react';
 
+class Button extends Component {
+  render(){
+    console.log('ejecutando render de button')
+    return (
+      <button>Enviar</button>
+    );
+  }
+}
+
 class App extends Component {
   state = {
     valor:3
@@ -10,11 +19,12 @@ class App extends Component {
     return(
       <div>
         <p>Hola mundo</p>
+        <Button />
         <button className={`${this.state.valor}`} onClick={()=> this.setState(
           {
             valor: 2
           })}>
-          Enviar
+          Enviar en App
         </button>
       </div>
     );
