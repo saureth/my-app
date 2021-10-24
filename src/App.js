b877f06dc040e13297f2c57460b776812cae0c04
Santiago Sanmartín <saureth001@gmail.com>
2021-10-23 19:02:50 -0500

    Clase 29

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import Button from "./button";
+import { Component } from 'react';
 
-const arr=[
-  'Chanchito feliz',
-  'Chanchito triste',
-  'Chanchito emocionado'
-];
-const App = ()=>{
-  const miVariable = false;
-  if(miVariable){
-    return <p>Variable en {miVariable.toString()}</p>
+class App extends Component {
+  render(){
+    return(
+      <p>
+        Hola mundo
+      </p>
+    );
   }
-  return (
-    <div>
-      <h1 onClick={(e)=>console.log('click', e)}>Hola Mundo</h1>
-      {arr.map(el => <p key={el}>{el}</p>)}
-      <Button onClick={()=>console.log('clickeado')}>
-        Enviar
-      </Button>
-    </div>
-  );
-};
+}
+
 export default App;
